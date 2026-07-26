54ef5e0e53912318058f4a29bd382f5343ff27a5
zimavi <bar05022009@gmail.com>
2026-07-26 14:52:49 +0300

    feat: add configuration class

---
+++ src/Yukihana.BuildConfig/Configuration.cs
@@ -0,0 +1,13 @@
+// Yukihana OS 2026 Yukihana OS Contributors
+// Licensed under the Apache License, Version 2.0. See LICENSE for details.
+
+namespace Yukihana.BuildConfig;
+
+/// <summary>
+/// This class hold tool's configuration like paths
+/// Edit this, to change some behaviour to what suits you
+/// </summary>
+public static class Configuration
+{
+
+}
